f672f5c815ec1fc30a83c5d30768e85e4761d274
AbhijithTyss <abhijith.styss@gmail.com>
2025-04-17 11:39:22 +0530

    Sync fork class added

---
+++ GitHubPractise/src/test/java/github/SyncFork.java
@@ -0,0 +1,11 @@
+package github;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SyncFork {
+	@Test
+	public void sync() {
+		Reporter.log("Sync fork class added",true);
+	}
+}
